db549b68bc1d42a1209d7b8a3f6eb325e5aa5cff
ahuby09 <ahuby100@hotmail.co.uk>
2024-02-10 19:56:36 +0000

    Adding inital commit
    homepage still a wip

---
+++ resources/views/index.blade.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TheWorldOfAutism</title>
+    <link href="/css/app.css" rel="stylesheet">
+    <style></style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light px-5">
+      <a class="navbar-brand" href="#">
+        <img src="{{ asset('images/logo.png') }}" alt="description of myimage">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between align-items-center" id="navbarNav">
+        <div class="navbar-nav mr-auto">
+          <!-- Empty div for logo on the left -->
+        </div>
+        <div>
+          <ul class="navbar-nav justify-content-end" style="padding-top: 10px;">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Merchandise</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">News</a>
+            </li>
+            <div class="top-nav  d-flex align-items-center">
+              <li class="nav-item pe-3">
+                <div class="form-check form-switch">
+                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                  <label class="form-check-label" for="flexSwitchCheckDefault">colour mode</label>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pe-0" href="#">Register</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+            </div>
+          </ul>
+          <ul class="navbar-nav fs-5">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Applications</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Activities</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Learning Environment</a>
+            </li>
+            <!-- Mega Menu -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="megaMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">Resources</a>
+              <div class="dropdown-menu mega-menu" aria-labelledby="megaMenu">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="mega-menu-column">
+                        <h5>United Kingdom</h5>
+                        <ul>
+                          <li>
+                            <a href="#">Link 1</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 2</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 3</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 4</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="mega-menu-column">
+                        <h5>United States of America</h5>
+                        <ul>
+                          <li>
+                            <a href="#">Link 5</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 6</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 7</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 8</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="mega-menu-column">
+                        <h5>South Africa</h5>
+                        <ul>
+                          <li>
+                            <a href="#">Link 9</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 10</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 11</a>
+                          </li>
+                          <li>
+                            <a href="#">Link 12</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <!-- End Mega Menu -->
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 600px; background: #9ED4B7; position: relative;">
+      <div class="text-center text-light" style="margin-top: -120px;">
+        <h1 class="fs-1">About In The World of Autism</h1>
+        <p style="max-width: 600px">Welcome to our Children's World! We're here to let you in and find The Resources, Connections, Fun Activities, Apps, Learning, and more that we have found and want to share. Let's make your life easier with all the information in just a few clicks!</p>
+      </div>
+      <div class="custom-shape-divider-bottom-1707484037" style="position: absolute; bottom: 0; left: 0; width: 100%;">
+        <svg class="fill" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+      </div>
+    </div>
+    <div class="container" style="margin-top: 30px;">
+      <!-- Image on the left, text on the right (default) -->
+      <div class="row col-md-12 min-vh-40 align-items-center pb-4 pt-4">
+        <div class="col-md-5">
+          <img class="mw-100" src="{{ asset('images/image-split-1.jpg') }}" alt="description of myimage">
+        </div>
+        <div class="col-md-7 d-flex flex-column ms-auto justify-content-center" style="max-width: 600px;">
+          <h2 class="text-left" style="max-width: 600px;text-align: left !important;color: #73B7C9;">Getting diagnosed</h2>
+          <p class="" style="max-width: 600px"> Receiving a diagnosis can evoke a range of emotions, and seeking support from loved ones and professionals is crucial during this challenging time. Parents often have conflicting emotions when navigating the diagnosis process, and each person's journey is unique. It's essential to acknowledge that while there may not be a definitive cure, obtaining a diagnosis marks the initial stride towards ensuring your child receives the necessary support to thrive and seize opportunities in life</p>
+        </div>
+      </div>
+      <!-- Image on the right, text on the left (reversed order on mobile) -->
+      <div class="row col-md-12 align-items-center min-vh-40 pb-5  pt-4">
+        <div class="col-md-5 order-md-2 ms-auto">
+          <img class="mw-100" src="{{ asset('images/image-split-2.jpeg') }}" alt="description of myimage">
+        </div>
+        <div class="col-md-7 order-md-1 justify-content-center" style="max-width: 600px;">
+          <h2 style="color: #73B7C9 !important;">Steps for help</h2>
+          <p>Discovering effective strategies and resources can significantly improve the quality of life for individuals with autism spectrum disorder (ASD). From early intervention programs to specialized therapies and educational support, finding the right combination of tools is essential. Our platform provides invaluable guidance and practical advice to navigate this journey, offering insights into creating supportive environments, fostering communication skills, and promoting social interaction. With the right steps and resources, individuals with autism can thrive and reach their full potential.</p>
+        </div>
+      </div>
+    </div>
+    <div class="custom-shape-divider-bottom-1707520980">
+      <svg class="fill-blue" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <div class="text-center text-light d-flex align-items-center justify-content-center" style="min-height: 400px; background: #73B7C9; position: relative;">
+      <figure class="text-center mb-0">
+        <blockquote class="blockquote">
+          <p class="pb-3">
+            <i class="fas fa-quote-left fa-xs text-primary"></i>
+            <span class="lead font-italic">
+              <h3>Why fit in when you were born to stand out?</h3>
+            </span>
+            <i class="fas fa-quote-right fa-xs text-primary"></i>
+          </p>
+        </blockquote>
+        <figcaption class="blockquote-footer mb-0 text-light"> Dr. Seuss </figcaption>
+      </figure>
+    </div>
+    <div class="text-center text-dark d-flex align-items-center pb-5 pt-5 justify-content-center" style="min-height: 400px; background: #FFF; position: relative;">
+      <div class="container" style="margin-top: 30px;">
+        <div class="row col-md-12 min-vh-40 pb-5 pt-4 align-items-center">
+          <div class="col-md-5">
+            <img class="mw-100" src="{{ asset('images/image-split-1.jpg') }}" alt="description of myimage">
+          </div>
+          <div class="col-md-7 d-flex flex-column ms-auto" style="max-width: 600px;">
+            <h2 class="text-left" style="max-width: 600px;text-align: left !important;">Water Play | Dump paint in the sink and learn colors! </h2>
+              <p style="max-width: 600px;text-align: left;">SENSORY PLAY WITH LEARNING
+              Different colored paint in the sink  |  Learn different colors and what multiple colors together make. Throw some toys in the sink for added learning! (Shapes, numbers, color toys) Easy clean up too! Watch videos below for demonstration!<br /><a href="https://www.tiktok.com/t/ZT83rHkBJ/"> Mama Doubois demo video 1 </a><br/><a href="https://www.tiktok.com/t/ZT83hkEnK/">Mama Doubois demo video 2</a>
+              </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="background: #9ED4B7;min-height: 75vh;align-items-center justify-content-center" class="d-flex align-items-center justify-content-center">
+      <div class="container">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <h2 class="text-light fs-1" style="text-align: center;margin-bottom: 30px;">FAQ</h2>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne"> Accordion Item #1 </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body. </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo"> Accordion Item #2 </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content. </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree"> Accordion Item #3 </button>
+            </h2>
+            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application. </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-white pt-5" style="background-color: #3e4551">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: Links -->
+        <section class="">
+          <!--Grid row-->
+          <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">FOOTER CONTENT</h5>
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi cum ipsam ad, illo possimus laborum ut reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque? Reiciendis assumenda iusto sapiente inventore animi? </p>
+            </div>
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Links</h5>
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Links</h5>
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Links</h5>
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Links</h5>
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" class="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </section>
+        <!-- Section: Links -->
+        <hr class="mb-4" />
+        <!-- Section: CTA -->
+        <section class="">
+          <p class="d-flex justify-content-center align-items-center">
+            <span class="me-3">Register for free</span>
+            <button type="button" class="btn btn-outline-light btn-rounded"> Sign up! </button>
+          </p>
+        </section>
+        <!-- Section: CTA -->
+        <hr class="mb-4" />
+        <!-- Section: Social media -->
+        <section class="mb-4 text-center">
+          <!-- Facebook -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <!-- Twitter -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <!-- Google -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+            <i class="fab fa-google"></i>
+          </a>
+          <!-- Instagram -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <!-- Linkedin -->
+          <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </section>
+        <!-- Section: Social media -->
+      </div>
+      <!-- Grid container -->
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)"> © 2020 Copyright: In The World of Autism </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+    <!-- End of .container -->
+    <script src="/js/app.js"></script>
+  </body>
+</html>

3b2ab54c9823ea5d04eb8dbe52f491c757bfbc04
ahuby09 <ahuby100@hotmail.co.uk>
2024-02-11 01:06:25 +0000

    Added hot bar and dynamic icons to sound board

--- resources/views/applications/sound.blade.php
+++ resources/views/applications/sound.blade.php
@@ -11,12 +11,37 @@
             display: flex;
             flex-wrap: wrap;
             justify-content: center;
+            margin-bottom: 20px;
         }
 
         .sound-icons .icon {
             font-size: 3rem;
             margin: 10px;
             cursor: pointer;
+        }
+        
+        .nav-options {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+        
+        .nav-options a {
+            margin-right: 20px;
+            text-decoration: none;
+            color: black;
+            cursor: pointer;
+        }
+        
+        .content-container {
+            text-align: center;
+        }
+
+        /* Style for inline icons */
+        .inline-icon {
+            display: inline-block;
+            margin-right: 10px;
+            font-size: 2rem;
         }
     </style>
 </head>
@@ -32,10 +57,28 @@
     <div class="icon" onclick="speak('I want to listen to music')"><i class="fa-solid fa-music"></i></div>
 </div>
 
+<div class="nav-options">
+    <a href="#" onclick="showMusicOptions()">Music</a>
+    <a href="#" onclick="showHolidayOptions()">Holiday</a>
+</div>
+
+<div class="content-container" id="content-container">
+    <!-- Content will be dynamically changed here -->
+</div>
+
 <script>
     function speak(text) {
         var utterance = new SpeechSynthesisUtterance(text);
         speechSynthesis.speak(utterance);
+    }
+    
+    function showMusicOptions() {
+        document.getElementById('content-container').innerHTML = 
+        '<span class="inline-icon icon" onclick="speak(\'I want to listen to Rock\')"><i class="fa-regular fa-hand-back-fist"></i></span><span class="inline-icon icon" onclick="speak(\'I want to listen to guitar\')"><i class="fa-solid fa-guitar"></i></span>';
+    }
+    
+    function showHolidayOptions() {
+        document.getElementById('content-container').innerHTML = '<span class="inline-icon icon" onclick="speak(\'Merry Christmas\')"><i class="fa-solid fa-gift"></i></span><span class="inline-icon icon" onclick="speak(\'Happy Easter\')"><i class="fa-solid fa-egg"></i></span>';
     }
 </script>
 </body>
